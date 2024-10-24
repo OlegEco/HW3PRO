821273f9cd7b86f83cfde5d0767075d1d1970685
OlegEco <stalkerkrutoj@gmail.com>
2024-10-24 10:04:38 +0200

    Add project files.

---
+++ HW3PRO/Controllers/PersonController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HW3PRO.Controllers
+{
+    public class PersonController : Controller
+    {
+        [HttpGet]
+        // Get: PersonController/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        // GET: PersonController/About
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        // GET: PersonController/Hobby
+        public ActionResult Hobby()
+        {
+            return View();
+        }
+    }
+}
