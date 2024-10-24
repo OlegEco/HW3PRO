6b575977df4c50d0d6675032b2e4f526ec11b473
OlegEco <stalkerkrutoj@gmail.com>
2024-10-24 10:25:48 +0200

    Refactor: Introduced PersonModel, updated controller to pass model data to views

--- HW3PRO/Controllers/PersonController.cs
+++ HW3PRO/Controllers/PersonController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HW3PRO.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HW3PRO.Controllers
@@ -9,21 +10,39 @@ namespace HW3PRO.Controllers
         // Get: PersonController/Index
         public ActionResult Index()
         {
-            return View();
+            var person = new PersonModel
+            {
+                Name = "Jack Sparrow",
+                PhotoUrl = "https://lumiere-a.akamaihd.net/v1/images/bluesteel_d0f846ee.jpeg",
+                GroupNumber = "P7",
+                ShortInfo = "I'm studyng to be a pirate at 'BLackbeard University'.",
+                Hobby = "I like to capture ships and hold seaports.",
+                AdditionalInfo = "Particularly talanted and dangerous Pirate."
+            };
+            return View(person);
         }
 
         [HttpGet]
         // GET: PersonController/About
         public ActionResult About()
         {
-            return View();
+            var person = new PersonModel()
+            {
+                AdditionalInfo = "Particularly talanted and dangerous Pirate."
+            };
+            return View(person);
         }
 
         [HttpGet]
         // GET: PersonController/Hobby
         public ActionResult Hobby()
         {
-            return View();
+            var person = new PersonModel()
+            {
+                Hobby = "I like to capture ships and hold seaports.",
+                Name = "Jack Sparrow"
+            };
+            return View(person);
         }
     }
 }
---
+++ HW3PRO/Models/PersonModel.cs
@@ -0,0 +1,12 @@
+﻿namespace HW3PRO.Models
+{
+    public class PersonModel
+    {
+        public string Name { get; set; }
+        public string PhotoUrl { get; set; }
+        public string GroupNumber { get; set; }
+        public string ShortInfo { get; set; }
+        public string Hobby { get; set; }
+        public string AdditionalInfo { get; set; }
+    }
+}
